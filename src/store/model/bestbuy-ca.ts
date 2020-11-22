9b42a4dbaa08fea76fe1e7c83e037d1cac2e7858
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2020-11-21 21:33:58 -0800

    feat(store): add ps5 bestbuy-ca pages

--- src/store/model/bestbuy-ca.ts
+++ src/store/model/bestbuy-ca.ts
@@ -110,7 +110,21 @@ export const BestBuyCa: Store = {
 			series: '3070',
 			url:
 				'https://www.bestbuy.ca/en-ca/product/nvidia-geforce-rtx-3070-8gb-gddr6-video-card-only-at-best-buy/15078017?intl=nosplash'
-		}
+		},
+		{
+			brand: 'sony',
+			model: 'ps5 console',
+			series: 'sonyps5c',
+			url:
+				'https://www.bestbuy.ca/en-ca/product/playstation-5-console-online-only/14962185',
+		},
+		{
+			brand: 'sony',
+			model: 'ps5 digital',
+			series: 'sonyps5c',
+			url:
+				'https://www.bestbuy.ca/en-ca/product/playstation-5-digital-edition-console-online-only/14962184',
+		},
 	],
 	name: 'bestbuy-ca',
 	waitUntil: 'domcontentloaded'
